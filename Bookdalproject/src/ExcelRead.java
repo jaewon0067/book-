2a28cc7f197e8a7983d4ed7405f85e2d2af222e4
smhrd <smhrd@DESKTOP-21UGQGF>
2020-06-22 17:53:47 +0900

    너도북어지

--- Bookdalproject/src/ExcelRead.java
+++ Bookdalproject/src/ExcelRead.java
@@ -10,9 +10,9 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelRead {	
+public class ExcelRead {
 	public static void main(String[] args) {
-		
+
 		ArrayList<BookVO> booklist = new ArrayList<BookVO>();
 		String book_index = null;
 		String lib_location = null;
@@ -28,71 +28,75 @@ public class ExcelRead {
 		String price = null;
 		String sidebook = null;
 		String[] bookinfo = new String[13];
-		
+
 		try {
-			
-			// URL in = ExcelRead.class.getClass().getResource(".//excel_resource//testValue.xlsx");
+
+			// URL in =
+			// ExcelRead.class.getClass().getResource(".//excel_resource//testValue.xlsx");
 			// String path = in.getPath();
-			
-            FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\excel_resource\\testValue.xlsx");
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
- 
-            int rowindex=0;
-            int columnindex=0;
-            //시트 수 (첫번째에만 존재하므로 0을 준다)
-            //만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
-            XSSFSheet sheet=workbook.getSheetAt(0);
-            //행의 수
-            int rows=sheet.getPhysicalNumberOfRows();
-            for(rowindex=0;rowindex<rows;rowindex++){
-                //행을읽는다
-                XSSFRow row=sheet.getRow(rowindex);
-                if(row !=null){
-                    //셀의 수
-                    int cells=row.getPhysicalNumberOfCells();
-                    for(columnindex=0; columnindex<=cells; columnindex++){
-                        //셀값을 읽는다
-                        XSSFCell cell=row.getCell(columnindex);
-                        String value="";
-                        //셀이 빈값일경우를 위한 널체크
-                        if(cell==null){
-                            continue;
-                        }else{
-                            //타입별로 내용 읽기
-                            switch (cell.getCellType()){
-                            case XSSFCell.CELL_TYPE_FORMULA:
-                                value=cell.getCellFormula();
-                                break;
-                            case XSSFCell.CELL_TYPE_NUMERIC:
-                                value=cell.getNumericCellValue()+"";
-                                break;
-                            case XSSFCell.CELL_TYPE_STRING:
-                                value=cell.getStringCellValue()+"";
-                                break;
-                            case XSSFCell.CELL_TYPE_BLANK:
-                                value=cell.getBooleanCellValue()+"";
-                                break;
-                            case XSSFCell.CELL_TYPE_ERROR:
-                                value=cell.getErrorCellValue()+"";
-                                break;
-                            }
-                        }
-                        bookinfo[columnindex] = value;
-                        if(columnindex == 12) {
-                        	
-                        BookVO vo = new BookVO(bookinfo[0], bookinfo[1], bookinfo[2], bookinfo[3], bookinfo[4], bookinfo[5], bookinfo[6], bookinfo[7], bookinfo[8], bookinfo[9], bookinfo[10], bookinfo[11], bookinfo[12]);
-            			booklist.add(vo);
+
+			FileInputStream file = new FileInputStream(
+					System.getProperty("user.dir") + "\\src\\excel_resource\\testValue.xlsx");
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			int rowindex = 0;
+			int columnindex = 0;
+			// 시트 수 (첫번째에만 존재하므로 0을 준다)
+			// 만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			// 행의 수
+			int rows = sheet.getPhysicalNumberOfRows();
+			for (rowindex = 0; rowindex < rows; rowindex++) {
+				// 행을읽는다
+				XSSFRow row = sheet.getRow(rowindex);
+				if (row != null) {
+					// 셀의 수
+					int cells = row.getPhysicalNumberOfCells();
+					for (columnindex = 0; columnindex <= cells; columnindex++) {
+						// 셀값을 읽는다
+						XSSFCell cell = row.getCell(columnindex);
+						String value = "";
+						// 셀이 빈값일경우를 위한 널체크
+						if (cell == null) {
+							continue;
+						} else {
+							// 타입별로 내용 읽기
+							switch (cell.getCellType()) {
+							case XSSFCell.CELL_TYPE_FORMULA:
+								value = cell.getCellFormula();
+								break;
+							case XSSFCell.CELL_TYPE_NUMERIC:
+								value = cell.getNumericCellValue() + "";
+								break;
+							case XSSFCell.CELL_TYPE_STRING:
+								value = cell.getStringCellValue() + "";
+								break;
+							case XSSFCell.CELL_TYPE_BLANK:
+								value = cell.getBooleanCellValue() + "";
+								break;
+							case XSSFCell.CELL_TYPE_ERROR:
+								value = cell.getErrorCellValue() + "";
+								break;
+							}
 						}
-                        
-                    }
- 
-                }
-            }
- 
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-		//System.out.println(booklist.get(20000).getOriginal());
+						bookinfo[columnindex] = value;
+						if (columnindex == 12) {
+
+							BookVO vo = new BookVO(bookinfo[0], bookinfo[1], bookinfo[2], bookinfo[3], bookinfo[4],
+									bookinfo[5], bookinfo[6], bookinfo[7], bookinfo[8], bookinfo[9], bookinfo[10],
+									bookinfo[11], bookinfo[12]);
+							booklist.add(vo);
+						}
+
+					}
+
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// System.out.println(booklist.get(20000).getOriginal());
 	}
 
 }
