c11ce4c722c77b4a4764856e62fcde5dd5b4987d
smhrd <smhrd@DESKTOP-21UGQGF>
2020-06-23 20:52:11 +0900

    sangrok->book1

--- Bookdalproject/src/View/SangrokGUI.java
+++ Bookdalproject/src/View/SangrokGUI.java
@@ -14,12 +14,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 import Controller.BookVO;
 import Controller.LibraryManagementSystem;
 import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import java.awt.GridBagLayout;
@@ -52,6 +54,7 @@ public class SangrokGUI {
 	 */
 	public SangrokGUI() {
 		initialize();
+		
 	}
 
 	/**
@@ -63,7 +66,7 @@ public class SangrokGUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SpringLayout springLayout = new SpringLayout();
 		frame.getContentPane().setLayout(springLayout);
-		
+
 		JPanel panel = new JPanel();
 		springLayout.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.NORTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, panel, 10, SpringLayout.WEST, frame.getContentPane());
@@ -71,12 +74,12 @@ public class SangrokGUI {
 		springLayout.putConstraint(SpringLayout.EAST, panel, 364, SpringLayout.WEST, frame.getContentPane());
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		search = new JTextField();
 		search.setBounds(0, 0, 305, 34);
 		panel.add(search);
 		search.setColumns(10);
-		
+
 		JButton btn_search = new JButton("\u261E");
 		btn_search.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -84,46 +87,38 @@ public class SangrokGUI {
 				ArrayList<BookVO> searchresult = controller.getSearchBook(searchbook);
 				for (int j = 0; j < searchresult.size(); j++) {
 					System.out.println(searchresult.get(j).getBook_name());
-					
+
 				}
-			
+
 			}
 		});
 		btn_search.setBounds(305, 0, 49, 34);
 		btn_search.setFont(new Font("±¼¸²", Font.PLAIN, 16));
 		panel.add(btn_search);
-		
-		
-		JScrollBar scrollBar = new JScrollBar();
-		springLayout.putConstraint(SpringLayout.NORTH, scrollBar, 6, SpringLayout.SOUTH, panel);
-		springLayout.putConstraint(SpringLayout.SOUTH, scrollBar, 497, SpringLayout.SOUTH, panel);
-		springLayout.putConstraint(SpringLayout.EAST, scrollBar, 0, SpringLayout.EAST, frame.getContentPane());
-		frame.getContentPane().add(scrollBar);
-		
+
 		JPanel bookrecommend = new JPanel();
 		springLayout.putConstraint(SpringLayout.WEST, bookrecommend, 10, SpringLayout.WEST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, bookrecommend, -10, SpringLayout.SOUTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, bookrecommend, -16, SpringLayout.WEST, scrollBar);
+		springLayout.putConstraint(SpringLayout.EAST, bookrecommend, 0, SpringLayout.EAST, panel);
+
 		frame.getContentPane().add(bookrecommend);
-		
+
 		JPanel libinfo = new JPanel();
 		springLayout.putConstraint(SpringLayout.WEST, libinfo, 10, SpringLayout.WEST, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, libinfo, -16, SpringLayout.WEST, scrollBar);
+		springLayout.putConstraint(SpringLayout.EAST, libinfo, -33, SpringLayout.EAST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, bookrecommend, 32, SpringLayout.SOUTH, libinfo);
 		springLayout.putConstraint(SpringLayout.SOUTH, libinfo, 158, SpringLayout.SOUTH, panel);
 		springLayout.putConstraint(SpringLayout.NORTH, libinfo, 6, SpringLayout.SOUTH, panel);
 		frame.getContentPane().add(libinfo);
-		
-	
-		
+
 		JLabel lblNewLabel = new JLabel("Librarian's PICK");
 		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 6, SpringLayout.SOUTH, libinfo);
 		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 0, SpringLayout.WEST, panel);
 		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, -6, SpringLayout.NORTH, bookrecommend);
-		
+
 		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, -20, SpringLayout.EAST, libinfo);
 		bookrecommend.setLayout(new GridLayout(3, 0, 0, 0));
-		
+
 		URL url = this.getClass().getResource("../icon/book1.PNG");
 		JButton btnbook1 = new JButton(new ImageIcon(url.getPath()));
 		btnbook1.setBorderPainted(false);
@@ -132,12 +127,14 @@ public class SangrokGUI {
 		btnbook1.setOpaque(false);
 		btnbook1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				book1GUI book1gui = new book1GUI();
+				book1gui.setNum(2);
+				frame.setVisible(true);
+
 			}
 		});
 		bookrecommend.add(btnbook1);
-	
-		
-				
+
 		URL url4 = this.getClass().getResource("../icon/book2.PNG");
 		JButton btnbook2 = new JButton(new ImageIcon(url4.getPath()));
 		btnbook2.setBorderPainted(false);
@@ -146,11 +143,15 @@ public class SangrokGUI {
 		btnbook2.setOpaque(false);
 		btnbook2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				book1GUI book1gui = new book1GUI();
+				book1gui.setNum(8);
+				frame.setVisible(true);
+				
 			}
 		});
 		bookrecommend.add(btnbook2);
-		
-		URL url1 = this.getClass().getResource("../icon/book3.PNG"); 
+
+		URL url1 = this.getClass().getResource("../icon/book3.PNG");
 		JButton btnbook3 = new JButton(new ImageIcon(url1.getPath()));
 		btnbook3.setBorderPainted(false);
 		btnbook3.setContentAreaFilled(false);
@@ -158,16 +159,19 @@ public class SangrokGUI {
 		btnbook3.setOpaque(false);
 		btnbook3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				book1GUI book1gui = new book1GUI();
+				book1gui.setNum(5);
+				frame.setVisible(true);
 			}
 		});
 		bookrecommend.add(btnbook3);
-		
+
 		libinfo.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		URL url5 = this.getClass().getResource("../icon/sangrok.PNG"); 
+
+		URL url5 = this.getClass().getResource("../icon/sangrok.PNG");
 		JLabel lib_info = new JLabel(new ImageIcon(url5.getPath()));
 		libinfo.add(lib_info);
-		
+
 		lblNewLabel.setFont(new Font("Ink Free", Font.BOLD, 22));
 		frame.getContentPane().add(lblNewLabel);
 	}
--- Bookdalproject/src/View/book1GUI.java
+++ Bookdalproject/src/View/book1GUI.java
@@ -13,73 +13,79 @@ import javax.swing.JLabel;
 
 public class book1GUI {
 
-	private JFrame frame;
+	public JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					book1GUI window = new book1GUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	private int num;
+	String path = null;
+
+	public void setNum(int num) {
+		this.num = num;
+		System.out.println(num);
+
+		if (num == 1) {
+			path = "../icon/bookinfo1.png";
+		} else if (num == 2) {
+			path = "../icon/bookinfo2.png";
+		} else if (num == 3) {
+			path = "../icon/bookinfo3.png";
+		} else if (num == 4) {
+			path = "../icon/bookinfo4.png";
+		} else if (num == 5) {
+			path = "../icon/bookinfo5.png";
+		} else if (num == 6) {
+			path = "../icon/bookinfo6.png";
+		} else if (num == 7) {
+			path = "../icon/bookinf7.png";
+		} else if (num == 8) {
+			path = "../icon/bookinfo8.png";
+		}
+
+		URL book_info1 = this.getClass().getResource(path);
+		Image originImg = new ImageIcon(book_info1.getPath()).getImage();
+		originImg = originImg.getScaledInstance(374, 561, Image.SCALE_SMOOTH);
+		ImageIcon Icon = new ImageIcon(originImg);
+
+		JLabel bookInfo_label = new JLabel(Icon);
+		bookInfo_label.setBounds(0, 0, 374, 561);
+		frame.getContentPane().add(bookInfo_label);
+		frame.setVisible(true);
+
+		
+		
+		
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public book1GUI() {
 		initialize();
+	
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 390, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
-		URL book_info1 = this.getClass().getResource("../icon/bookinfo1.png");
-		Image originImg = new ImageIcon(book_info1.getPath()).getImage();
-		originImg = originImg.getScaledInstance(374, 561, Image.SCALE_SMOOTH);
-		ImageIcon Icon = new ImageIcon(originImg);
-
+		
 		JButton rental_btn = new JButton("");
 		rental_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-
+			public void actionPerformed(ActionEvent e) {
+				LibraryRentalGUI.main(null);
 			}
 		});
+		rental_btn.setBounds(63, 504, 256, 23);
+		frame.getContentPane().add(rental_btn);
+
+		// 대출하기 버튼 안보이게
 		rental_btn.setContentAreaFilled(false);
 		rental_btn.setFocusPainted(false);
 		rental_btn.setBorderPainted(false);
 		rental_btn.setBounds(65, 504, 257, 23);
 		frame.getContentPane().add(rental_btn);
-		
-		JLabel bookInfo_label = new JLabel(Icon);
-		
-		bookInfo_label.setBounds(0, 0, 374, 561);
-		frame.getContentPane().add(bookInfo_label);
-
-//		URL url = this.getClass().getResource("../icon/book1.PNG");
-//		JButton btnbook1 = new JButton(new ImageIcon(url.getPath()));
-//		btnbook1.setBorderPainted(false);
-//		btnbook1.setContentAreaFilled(false);
-//		btnbook1.setFocusPainted(false);
-//		btnbook1.setOpaque(false);
-//		btnbook1.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//			}
-//		});
-//		bookrecommend.add(btnbook1);
 
 	}
 }
